f353bad73510f9a0813b12b89acc01d562263d35
PengendaliTanah <86563651+PengendaliTanah@users.noreply.github.com>
2022-12-09 11:58:14 +0700

    backend

---
+++ login menuju home/index.php
@@ -0,0 +1,147 @@
+<?php
+require 'system/config/koneksi.php'
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Beranda</title>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Contrail+One|Raleway" rel="stylesheet">
+  <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+  <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
+  <link rel="shortcut icon" href="asset/internal/img/img-local/favicon.ico">
+  <link rel="stylesheet" href="asset/internal/css/style-index1.css">
+  <link rel="stylesheet" href="asset/internal/css/style-index2.css">
+
+  <script src="asset/internal/js/preloader.js" type="text/javascript"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+  <script>
+    $(document).ready(function() {
+      $(".preloader").fadeOut();
+    })
+  </script>
+
+</head>
+
+<body>
+
+  <!--Pre Loader-->
+  <div class="preloader">
+    <div class="loading">
+      <img src="asset/internal/img/img-local/spiner.gif" width="80">
+    </div>
+  </div>
+
+
+  <!--Navbar-->
+  <header>
+    <a href="#" id="logo"></a>
+    <nav>
+      <a href="#" id="menu-icon"></a>
+      <ul id="top-menu">
+        <li style="list-style: none; display: inline"></li>
+        <li class="active">
+          <a href="#">Beranda</a>
+        </li>
+        <li style="list-style: none; display: inline"></li>
+        <li>
+          <a href="#foo">Petunjuk</a>
+        </li>
+        <li style="list-style: none; display: inline"></li>
+      </ul>
+    </nav>
+  </header>
+
+  <!-- konten1 -->
+  <div class="page-wrap">
+    <div class="header">
+      <div class="box-1">
+        <h1 disabled>Bank Sampah Trash-Mate</h1>
+        <p> Ayo menjadi bagian dari agen of change dengan menukaran sampahmu menjadi cuan bersama Trash-Mate!!! </p>
+        <br> <br>
+
+        <div class="center">
+          <a href="page/login.php" target="_blank">
+            <div class="btn" align="center">Login</div>
+          </a> <!-- End Btn -->
+
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+
+  <!--konten2-->
+  <div id="foo">
+    <section class="team">
+      <div class="container">
+        <div class="row">
+          <h1>MARI BERGABUNG !!!</h1>
+          <p>Bank sampah trash-Mate adalah organisasi peduli lingkungan yang berlokasi di Politeknik Negeri Lampung dan penduduk sekitar Kota Bandar Lampung. Selain itu Program bank sampah adalah bentuk solusi yang diberikan pemerintah dalam rangka mengatasi masalah sampah yang ada di Indonesia. Adapun definisi serta fungsi dan tujuannya di antaranya sebagai berikut.</p>
+        </div>
+        <div class="row mgt50px">
+          <div class="coloumn">
+            <div class="imgBox">
+              <img src="asset/internal/img/img-content/1.jpg">
+            </div>
+            <div class="details">
+              <h3>#Langkah1<br><span>Lakukan Pendaftaran</span></h3>
+            </div>
+          </div>
+          <div class="coloumn">
+            <div class="imgBox">
+              <img src="asset/internal/img/img-content/2.jpg">
+            </div>
+            <div class="details">
+              <h3>#Langkah2<br><span>Pemilahan Sampah</span></h3>
+            </div>
+          </div>
+          <div class="coloumn">
+            <div class="imgBox">
+              <img src="asset/internal/img/img-content/3.jpg">
+            </div>
+            <div class="details">
+              <h3>#Langkah3<br><span>Penimbangan Sampah</span></h3>
+            </div>
+          </div>
+          <div class="coloumn">
+            <div class="imgBox">
+              <img src="asset/internal/img/img-content/4.jpg">
+            </div>
+            <div class="details">
+              <h3>#Langkah4<br><span>Mendapat Keuntungan</span></h3>
+            </div>
+          </div>
+          <div style="clear: both;"></div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src="asset/internal/js/index.js"></script>
+
+  <!--footer-->
+  <footer class="footer-distributed">
+
+    <div class="footer-left">
+
+      <a href="#" id="logo_f"></a>
+      <br>
+
+        <p><font color="white">Copyright &#169; <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script> <a expr:href='data:blog.homepageUrl'><data:blog.title/></a>. All rights reserved.</font></p>
+    </div>
+
+
+    </div>
+
+  </footer>
+
+</body>
+
+</html>
